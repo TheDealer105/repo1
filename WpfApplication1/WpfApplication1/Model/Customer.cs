32dcfe99920c9c991f4d40d645167536b1143296
Wojtyczka Tomasz <twojtyczka@hq.internetium.pl>
2016-06-15 11:11:29 +0200

    add bindablebase implementation in Customer class

--- WpfApplication1/WpfApplication1/Model/Customer.cs
+++ WpfApplication1/WpfApplication1/Model/Customer.cs
@@ -3,32 +3,77 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DevExpress.Mvvm;
 
 namespace WpfApplication1.Model
 {
-    public class Customer
+    public class Customer : BindableBase
     {
         public Customer()
         {
-
         }
 
-        public string Name { get; set; }
 
-        public string Surname { get; set; }
+        public string Name
+        {
+            get { return GetProperty(() => Name); }
+            set { SetProperty(() => Name, value); }
+        }
 
-        public DateTime? DateOfBirth { get; set; }
 
-        public string Adress { get; set; }
+        public string Surname
+        {
+            get { return GetProperty(() => Surname); }
+            set { SetProperty(() => Surname, value); }
+        }
 
-        public string City { get; set; }
 
-        public string PostalCode { get; set; }
+        public DateTime? DateOfBirth
+        {
+            get { return GetProperty(() => DateOfBirth); }
+            set { SetProperty(() => DateOfBirth, value); }
+        }
 
-        public string PhoneNumber { get; set; }
 
-        public string Email { get; set; }
+        public string Adress
+        {
+            get { return GetProperty(() => Adress); }
+            set { SetProperty(() => Adress, value); }
+        }
 
-        public double Value { get; set; }
+
+        public string City
+        {
+            get { return GetProperty(() => City); }
+            set { SetProperty(() => City, value); }
+        }
+
+
+        public string PostalCode
+        {
+            get { return GetProperty(() => PostalCode); }
+            set { SetProperty(() => PostalCode, value); }
+        }
+
+
+        public string PhoneNumber
+        {
+            get { return GetProperty(() => PhoneNumber); }
+            set { SetProperty(() => PhoneNumber, value); }
+        }
+
+
+        public string Email
+        {
+            get { return GetProperty(() => Email); }
+            set { SetProperty(() => Email, value); }
+        }
+
+
+        public double Value
+        {
+            get { return GetProperty(() => Value); }
+            set { SetProperty(() => Value, value); }
+        }
     }
 }
